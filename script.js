549da1c3e9d756dd8a0a905131d0603bc98ce489
KevinKaras <kevinkaras2@gmail.com>
2021-11-17 14:11:15 -0500

    added entire gamegrid with populated cells

--- script.js
+++ script.js
@@ -15,13 +15,41 @@ const controlsBar = {
     width: canvas.width,
     height: cellSize
 }
+class Cell {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+    }
+
+    draw(){
+        ctx.strokestyle = 'black'
+        ctx.strokeRect(this.x, this.y, this.width, this.height)
+
+    }
+}
+function createGrid(){
+    for(let y = cellSize; y < canvas.height; y += cellSize){
+        for(let x = 0; x < canvas.width; x += cellSize){
+            gameGrid.push(new Cell(x, y))
+        }
+    }
+}
+createGrid();
+function handleGameGrid(){
+    for(let i = 0; i < gameGrid.length; i++){
+        gameGrid[i].draw()
+    }
+}
 
 
 
 function animate(){
-    ctx.fillStyle = 'blue'
-    ctx.fillRect(0,0, controlsBar.width, controlsBar.height)
-    requestAnimationFrame(animate)
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0,0, controlsBar.width, controlsBar.height);
+    handleGameGrid();
+    requestAnimationFrame(animate);
 
 }
 animate()

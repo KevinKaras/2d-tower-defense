0fdb5385bafb7f44c860a61c9d568027ad18bf80
KevinKaras <kevinkaras2@gmail.com>
2021-11-18 13:09:34 -0500

    added defenders and rendering defenders concept

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ canvas.width = 900;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
+const defenders = [];
+let numberOfResources = 300;
 
 
 // mouse
@@ -44,8 +46,11 @@ class Cell {
     }
 
     draw(){
-        ctx.strokestyle = 'black'
-        ctx.strokeRect(this.x, this.y, this.width, this.height)
+        if(mouse.x && mouse.y && collision(this, mouse)){
+            ctx.strokestyle = 'black'
+            ctx.strokeRect(this.x, this.y, this.width, this.height)
+        }
+        
 
     }
 }
@@ -63,13 +68,64 @@ function handleGameGrid(){
     }
 }
 
+// defenders
+
+class Defender {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.shooting = false
+        this.health = 100;
+        this.projectiles = [];
+        this.timer = 0;
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(this.x, this.y, this.height, this.width);
+        ctx.fillStyle = 'gold';
+        ctx.font = '20px Arial';
+        ctx.fillText(Math.floor(this.health), this.x, this.y)
+
+    }
+}
+canvas.addEventListener('click', function(){
+    const gridPositionX = mouse.x - (mouse.x % cellSize);
+    const gridPositionY = mouse.y - (mouse.y % cellSize);
+    if(gridPositionY < cellSize) return;
+    let defenderCost = 100;
+    if(numberOfResources > defenderCost){
+        defenders.push(new Defender(gridPositionX, gridPositionY))
+        numberOfResources -= defenderCost
+    }
+})
+function handleDefenders(){
+    for(let i = 0; i < defenders.length; i ++){
+        defenders[i].draw()
+    }
+}
 
 
 function animate(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(0,0, controlsBar.width, controlsBar.height);
     handleGameGrid();
+    handleDefenders();
     requestAnimationFrame(animate);
 
 }
 animate()
+
+function collision(first, second){
+    if(
+        !( first.x > second.x + second.width  ||
+           first.x + first.width < second.x   ||
+           first.y > second.y + second.height ||
+           first.y + first.height < second.y)
+        
+    ){
+        return true;
+    };
+}
